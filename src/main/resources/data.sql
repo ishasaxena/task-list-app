481deb8a6142cb973812b30b3dc7e6fe548651d0
Isha Saxena <ishasaxena27@gmail.com>
2023-10-14 10:40:02 +0530

    Initial commit: Task List Application

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, email) VALUES (1, 'user1', 'user1@example.com');
+INSERT INTO users (id, username, email) VALUES (2, 'user2', 'user2@example.com');
+-- Add more dummy data as needed
